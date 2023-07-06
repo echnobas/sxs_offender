fe87a31d70cf22836839ab4544116eb812da86b2
echnobas <echnobas@pm.me>
2023-07-06 18:46:19 +0100

    fix bug nyaa

--- remove_defender.bat
+++ remove_defender.bat
@@ -76,6 +76,10 @@ $certPath = [System.IO.Path]::GetTempFileName()
 [System.IO.File]::WriteAllBytes($certPath, $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
 Import-Certificate $certPath -CertStoreLocation "Cert:\LocalMachine\Root" | Out-Null
 Copy-Item -Path "$certRegPath\$($cert.Thumbprint)" "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force  | Out-Null
-Add-WindowsPackage -Online -PackagePath $cabPath
+Add-WindowsPackage -Online -NoRestart -PackagePath $cabPath | Out-Null
 Get-ChildItem "Cert:\LocalMachine\Root\$($cert.Thumbprint)" | Remove-Item
-Remove-Item "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force | Out-Null
+Remove-Item "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force -Recurse | Out-Null
+Remove-Item $cabPath -Force | Out-Null
+Write-Host "Restart pending"
+pause
+shutdown /r /t 0
